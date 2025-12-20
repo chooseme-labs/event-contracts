32ff7b1c21a6c4383238bf884edcbe4544c50995
ggoz <963033921@qq.com>
2025-12-20 23:23:40 +0800

    order module

--- src/event/core/OrderBookManagerStorage.sol
+++ src/event/core/OrderBookManagerStorage.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.20;
 
 import "../../interfaces/event/IOrderBookManager.sol";
+import "../../interfaces/event/IOrderBookPod.sol";
 
 abstract contract OrderBookManagerStorage is IOrderBookManager {
-    constructor(){
+    mapping(IOrderBookPod => bool) public podIsWhitelisted;
 
-    }
+    mapping(uint256 => IOrderBookPod) public eventIdToPod;
+
+    uint256[99] private __gap;
 }

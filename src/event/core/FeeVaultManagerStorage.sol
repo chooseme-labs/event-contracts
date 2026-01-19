d21d3516ff70ba26cda095025701bfe97cc62a3e
stevemasonup-oss <steve.mason.up@gmail.com>
2026-01-19 17:05:49 +0800

    feat: Implement AdminFeeVault and FeeVaultPod with associated storage and management functionalities

--- src/event/core/FeeVaultManagerStorage.sol
+++ src/event/core/FeeVaultManagerStorage.sol
@@ -2,9 +2,11 @@
 pragma solidity ^0.8.20;
 
 import "../../interfaces/event/IFeeVaultManager.sol";
+import "../common/BaseManagerStorage.sol";
 
-abstract contract FeeVaultManagerStorage is IFeeVaultManager {
-    constructor(){
+abstract contract FeeVaultManagerStorage is BaseManagerStorage, IFeeVaultManager {
+    // Global AdminFeeVault address
+    address public adminFeeVault;
 
-    }
+    uint256[99] private __gap;
 }
--- src/event/pod/FeeVaultPodStorage.sol
+++ src/event/pod/FeeVaultPodStorage.sol
@@ -4,7 +4,26 @@ pragma solidity ^0.8.20;
 import "../../interfaces/event/IFeeVaultPod.sol";
 
 abstract contract FeeVaultPodStorage is IFeeVaultPod {
-    constructor(){
+    address public constant ETHAddress = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
 
-    }
+    // FeeVaultManager address
+    address public feeVaultManager;
+
+    // WithdrawManager address (can withdraw fees)
+    address public withdrawManager;
+
+    // AdminFeeVault address
+    address public adminFeeVault;
+
+    // Admin fee rate (basis points, e.g., 1000 = 10%)
+    uint256 public adminFeeRate;
+
+    // Token balances for each fee type
+    // token => feeType => balance
+    mapping(address => mapping(FeeType => uint256)) public feeBalances;
+
+    // Total token balances
+    mapping(address => uint256) public tokenBalances;
+
+    uint256[94] private __gap;
 }
